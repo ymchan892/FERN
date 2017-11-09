375cc5a142e1ba61f232a2c8dd97df1624c84aaa
Wang Shengeih <shengeih@gmail.com>
2017-11-09 15:26:38 +0800

    no use email

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('common_lib','email');
+$autoload['libraries'] = array('common_lib');
 
 /*
 | -------------------------------------------------------------------
