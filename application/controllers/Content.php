e3678a971f68e81bafa2544d7982cacc52bf2406
Wang Shengeih <shengeih@gmail.com>
2017-11-09 14:34:06 +0800

    add content pages

---
+++ application/controllers/Content.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Content extends CI_Controller
+{
+    public function privacy()
+    {
+        echo 'privacy';
+    }
+
+    public function terms()
+    {
+        echo 'terms';
+    }
+}
